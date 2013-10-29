bc78ae045a3510a66164ec1fdf51b2018d9566a0
Daniel Rodriguez <drodriguez@oculus360.us>
2013-10-29 13:34:57 -0500

    New 0.1 version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ subprocess.call(['make', '-C', 'word2vec-src'])
 
 setup(
     name='word2vec',
-    version='0.0.1',
+    version='0.1',
     maintainer='Daniel Rodriguez',
     maintainer_email='df.rodriguez143@gmail.com',
     url='https://github.com/danielfrg/word2vec',
