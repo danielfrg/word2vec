fb12426ef94822db24c86aaaa758568bfd62d0ed
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-24 17:54:42 -0600

    Added verbose option

--- word2vec/scripts_interface.py
+++ word2vec/scripts_interface.py
@@ -1,10 +1,11 @@
 # coding: utf-8
+import sys
 import subprocess
 
 
 def word2vec(train, output, size=100, window=5, sample=0, hs=1, negative=0, threads=4,
              min_count=5, alpha=0.025, debug=2, binary=1, cbow=0,
-             save_vocab=None, read_vocab=None):
+             save_vocab=None, read_vocab=None, verbose=False):
     process = ['word2vec']
     args = ['-train', '-output', '-size', '-window', '-sample', '-hs', '-negative', '-threads',
             '-min-count', '-alpha', '-debug', '-binary', '-cbow']
@@ -19,7 +20,18 @@ def word2vec(train, output, size=100, window=5, sample=0, hs=1, negative=0, thre
     if read_vocab is not None:
         process.append('-read-vocab')
         process.append(str(read_vocab))
-    subprocess.call(process)
+    proc = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if verbose:
+        for line in proc.stdout:
+            sys.stdout.write(line)
+            sys.stdout.flush()
+
+    out, err = proc.communicate()
+    if 'ERROR:' in out:
+        raise Exception(out)
+
+    # subprocess.call(process)
 
 
 def word2clusters(train, output, classes, size=100, window=5, sample=0, hs=1, negative=0, threads=4,
