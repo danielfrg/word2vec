c49c288a046df58961621d5d86901885e39241be
Daniel Rodriguez <drodriguez@oculus360.us>
2014-01-31 15:24:56 -0600

    Version 0.4.1 fixes the bug of not including the c source code

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ subprocess.call(['make', '-C', 'word2vec-c'])
 
 setup(
     name='word2vec',
-    version='0.4',
+    version='0.4.1',
     maintainer='Daniel Rodriguez',
     maintainer_email='df.rodriguez143@gmail.com',
     url='https://github.com/danielfrg/word2vec',
