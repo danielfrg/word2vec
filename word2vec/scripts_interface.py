51207081fcee8327b2f3e9d8ba6b6ec405209dee
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-03-25 17:30:03 -0500

    fix unicode error on word2phrase

--- word2vec/scripts_interface.py
+++ word2vec/scripts_interface.py
@@ -46,7 +46,7 @@ def word2vec(train, output, size=100, window=5, sample='1e-3', hs=0, negative=5,
         verbose
             Print output from training
     """
-    process = ['word2vec']
+    command = ['word2vec']
     args = ['-train', '-output', '-size', '-window', '-sample', '-hs',
             '-negative', '-threads', '-iter', '-min-count', '-alpha', '-debug',
             '-binary', '-cbow']
@@ -54,16 +54,16 @@ def word2vec(train, output, size=100, window=5, sample='1e-3', hs=0, negative=5,
               min_count, alpha, debug, binary, cbow]
 
     for arg, value in zip(args, values):
-        process.append(arg)
-        process.append(unicode(value))
+        command.append(arg)
+        command.append(unicode(value))
     if save_vocab is not None:
-        process.append('-save-vocab')
-        process.append(unicode(save_vocab))
+        command.append('-save-vocab')
+        command.append(unicode(save_vocab))
     if read_vocab is not None:
-        process.append('-read-vocab')
-        process.append(unicode(read_vocab))
+        command.append('-read-vocab')
+        command.append(unicode(read_vocab))
 
-    proc = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if verbose:
         for line in proc.stdout:
             sys.stdout.write(line)
@@ -78,23 +78,26 @@ def word2clusters(train, output, classes, size=100, window=5, sample='1e-3',
                   hs=0, negative=5, threads=12, iter_=5, min_count=5,
                   alpha=0.025, debug=2, binary=1, cbow=1,
                   save_vocab=None, read_vocab=None, verbose=False):
-    process = ['word2vec']
+    command = ['word2vec']
+
     args = ['-train', '-output', '-size', '-window', '-sample', '-hs',
             '-negative', '-threads', '-iter', '-min-count', '-alpha', '-debug',
             '-binary', '-cbow', '-classes']
     values = [train, output, size, window, sample, hs, negative, threads, iter_,
               min_count, alpha, debug, binary, cbow, classes]
-    for arg, value in zip(args, values):
-        process.append(arg)
-        process.append(unicode(value))
-    if save_vocab is not None:
-        process.append('-save-vocab')
-        process.append(unicode(save_vocab))
-    if read_vocab is not None:
-        process.append('-read-vocab')
-        process.append(unicode(read_vocab))
 
-    proc = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for arg, value in zip(args, values):
+        command.append(arg)
+        command.append(unicode(value))
+
+    if save_vocab is not None:
+        command.append('-save-vocab')
+        command.append(unicode(save_vocab))
+    if read_vocab is not None:
+        command.append('-read-vocab')
+        command.append(unicode(read_vocab))
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if verbose:
         for line in proc.stdout:
@@ -105,14 +108,16 @@ def word2clusters(train, output, classes, size=100, window=5, sample='1e-3',
 
 
 def word2phrase(train, output, min_count=5, threshold=100, debug=2, verbose=False):
-    process = ['word2phrase']
+    command = ['word2phrase']
+
     args = ['-train', '-output', '-min-count', '-threshold', '-debug']
     values = [train, output, min_count, threshold, debug]
     for arg, value in zip(args, values):
-        process.append(arg)
-        process.append(value)
+        command.append(arg)
+        command.append(unicode(value))
 
-    proc = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print command
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if verbose:
         for line in proc.stdout:
