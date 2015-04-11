c7739cd747ab750d2592bff7ab076c0a3ef5326d
Stephen A. Balaban <s@lambdal.com>
2015-04-11 16:39:36 -0700

    Used 2to3 for python3 support.

--- word2vec/__init__.py
+++ word2vec/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from io import *
-from wordvectors import *
-from wordclusters import *
-from scripts_interface import *
+from .io import *
+from .wordvectors import *
+from .wordclusters import *
+from .scripts_interface import *
