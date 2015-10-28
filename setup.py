d1a3856b471527f6f4aadd80076511da8fb34daf
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-10-28 17:42:36 -0500

    Update setup.py to versioner and other things

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
+"""
+To upload a new version:
+1. make clean
+2. git tag a new version: git tag v1.x.x
+3. python setup.py sdist
+4. python setup.py register upload
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+
 import os
 import sys
 import subprocess
-from distutils.core import setup
 
-'''
-To update to a new version:
-1. change version
-2. python setup.py sdist register upload
-'''
-
-DESCRIPTION = 'Google word2vec python wrapper'
+import versioneer
 
 SOURCES_DIR = 'word2vec-c'
 BIN_DIR = 'bin'
 if not os.path.exists(BIN_DIR):
     os.makedirs(BIN_DIR)
 
-
-def compile(source, target):
+def compile_c(source, target):
     CC = 'gcc'
     CFLAGS = ('-lm -pthread -O3 -Wall -march=native -funroll-loops '
               '-Wno-unused-result')
@@ -34,27 +37,29 @@ def compile(source, target):
     if return_code > 0:
         exit(return_code)
 
-compile('word2vec.c', 'word2vec')
-compile('word2phrase.c', 'word2phrase')
-compile('distance.c', 'word2vec-distance')
-compile('word-analogy.c', 'word2vec-word-analogy')
-compile('compute-accuracy.c', 'word2vec-compute-accuracy')
-compile('word2vec-sentence2vec.c', 'word2vec-doc2vec')
+compile_c('word2vec.c', 'word2vec')
+compile_c('word2phrase.c', 'word2phrase')
+compile_c('distance.c', 'word2vec-distance')
+compile_c('word-analogy.c', 'word2vec-word-analogy')
+compile_c('compute-accuracy.c', 'word2vec-compute-accuracy')
+compile_c('word2vec-sentence2vec.c', 'word2vec-doc2vec')
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='word2vec',
-    version='0.8',
-    maintainer='Daniel Rodriguez',
-    maintainer_email='df.rodriguez143@gmail.com',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Daniel Rodriguez',
+    author_email='df.rodriguez143@gmail.com',
     url='https://github.com/danielfrg/word2vec',
-    packages=['word2vec'],
-    description=DESCRIPTION,
+    description='Wrapper for Google word2vec',
     license='Apache License Version 2.0, January 2004',
+    packages=find_packages(),
     data_files=[('bin', ['bin/word2vec', 'bin/word2phrase',
                          'bin/word2vec-distance', 'bin/word2vec-word-analogy',
                          'bin/word2vec-compute-accuracy',
                          'bin/word2vec-doc2vec'])],
-    install_requires=[
-        'numpy>=1.9.2'
-    ],
+    install_requires=required
 )
