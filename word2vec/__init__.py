e45c32c447eee1ee238f9086a80d24489abbff92
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-10-28 17:41:59 -0500

    Add versioneer

--- word2vec/__init__.py
+++ word2vec/__init__.py
@@ -2,3 +2,7 @@ from .io import *
 from .wordvectors import *
 from .wordclusters import *
 from .scripts_interface import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
