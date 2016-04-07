4829a68ac2aadd88525e285bc2b936a3ea618199
Konstantinos Drossos <kostas.drosos@icloud.com>
2016-04-07 10:52:39 +0300

    Reformatted comments according to numpy style.

--- word2vec/wordvectors.py
+++ word2vec/wordvectors.py
@@ -42,18 +42,17 @@ class WordVectors(object):
         return self.vocab_hash[word]
 
     def word(self, ix):
-        """
+        """Returns the word that corresponds to the index.
 
-        ----
+        Parameters
+        -------
+        ix : int
+            The index of the word
 
-        Returns the word that corresponds to the index.
-
-        ----
-
-        :param ix: The index of the word
-        :type ix: int
-        :return: The word that corresponds to the index
-        :rtype: str
+        Returns
+        -------
+        str
+            The word that corresponds to the index
         """
         return self.vocab[ix]
 
@@ -71,18 +70,17 @@ class WordVectors(object):
         return self.vectors[idx]
 
     def get_word(self, vector):
-        """
+        """Returns the word according to the vector
 
-        ----
+        Parameters
+        -------
+        vector : numpy.core.multiarray.array
+            The representing vector of the word
 
-        Returns the word according to the vector
-
-        ----
-
-        :param vector: The representing vector of the word
-        :type vector: numpy.core.multiarray.array
-        :return: The word according to the specified vector if found, else None
-        :rtype: str|None
+        Returns
+        -------
+        str or None
+            The word according to the specified vector if found, else None
         """
         word_index = np.where(np.all(self.vectors == vector, axis=1))[0]
         return self.word(word_index[0]) if word_index.size else None
