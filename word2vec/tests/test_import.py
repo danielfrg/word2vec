8846faa266c7a88b3743115a51e5e218accac470
Daniel Rodriguez <daniel@danielfrg.com>
2020-05-06 14:50:08 -0500

    Derpy hooves

--- word2vec/tests/test_import.py
+++ word2vec/tests/test_import.py
@@ -2,5 +2,5 @@ def test_import():
     import word2vec
 
     assert word2vec.__version__ is not None
-    assert len(word2vec.__version__) > 0
     assert word2vec.__version__ != "0.0.0"
+    assert len(word2vec.__version__) > 0
